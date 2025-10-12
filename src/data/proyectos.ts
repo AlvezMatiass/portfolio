2e1c948fb2ea0c1c4f3275915ed7d9664ebab5e8
Alvez Matias <matias2018alvez@gmail.com>
2025-10-12 11:50:54 -0600

    LocalImage

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -3,7 +3,7 @@ export const proyectos = [
   {
     titulo: "My Party Rental Guys - 2025",
     url: "https://mypartyrentalguys.com/",
-    imagen: "https://i.ibb.co/G4PZdhT6/MPRG.png",
+    imagen: "/MPRG1ra.png",
     habilidades: ["Wordpress", "Elementor PRO", "HTML", "CSS", "JavaScript", "SEO", "YoastSEO", "SurferSEO", "Analisis Web", "Plugins", "Blog Post"],
     figmaName: "See Figma Prototype",
     link: "https://www.figma.com/design/4fd21coIFGl6Dul6wdfO5u/Untitled?node-id=137-5&t=0ZuIHiL7vsrYXYs6-1",
@@ -11,13 +11,13 @@ export const proyectos = [
   {
     titulo: "theKollab - 2025",
     url: "https://thekollab.io/",
-    imagen: "https://i.ibb.co/Q44tv1X/THEKOLLAB.png",
+    imagen: "/THEKOLLAB2da,png",
     habilidades: ["Freelance", "Wordpress", "Elementor PRO", "HTML", "CSS", "JavaScript", "SEO", "Rank Math SEO", "Plugins", "Blog Post", "Error Correction"],
   },
   {
     titulo: "Haz&Enves - 2023",
     url: "https://hazyenves.netlify.app/",
-    imagen: "https://i.ibb.co/xS7p0ktS/HAZYENVES.png",
+    imagen: "/HAZYENVES3ra.png",
     habilidades: ["HTML", "CSS", "JavaScript", "ReactJS", "ReactDOM", "SASS", "Vite", "Tailwind CSS"],
     figmaName: "See Figma Prototype",
     link: "https://www.figma.com/design/DY9ZIgcPBO1fZ8bz7Vl8TU/H-E-Dise%C3%B1os-Naturales?node-id=60-4&t=eFHzPoezdDWM7kKl-1",
@@ -25,7 +25,7 @@ export const proyectos = [
   {
     titulo: "Audio&Video MPRG - 2025",
     url: "https://avrentalsmprg.com/",
-    imagen: "https://i.ibb.co/GffV3vXP/AVMRPG.png",
+    imagen: "/AVMRPG4ta.png",
     habilidades: ["Wordpress", "Elementor PRO", "YoastSEO", "Analisis Web", "SEO", "Plugins", "Diseño Web"],
     figmaName: "See Figma Prototype",
     link: "https://www.figma.com/design/4fd21coIFGl6Dul6wdfO5u/Untitled?node-id=137-5&t=0ZuIHiL7vsrYXYs6-1",
@@ -33,7 +33,7 @@ export const proyectos = [
   {
     titulo: "Mobile Bartending MPRG - 2025",
     url: "https://mobilebartendingmprg.com/",
-    imagen: "https://i.ibb.co/b5w3k9S2/MBMPRG.png",
+    imagen: "/MBMPRG5ta.png",
     habilidades: ["Wordpress", "Elementor PRO", "YoastSEO", "Analisis Web", "SEO", "Plugins", "Diseño Web"],
     figmaName: "See Figma Prototype",
     link: "https://www.figma.com/design/4fd21coIFGl6Dul6wdfO5u/Untitled?node-id=137-5&t=0ZuIHiL7vsrYXYs6-1",
